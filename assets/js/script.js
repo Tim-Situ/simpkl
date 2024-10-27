2ca778f457ce0aaaf485f1cea406598284c709e3
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-10-27 18:22:20 +0700

    feat: add responsive menu and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  $(".menu-toggle").click(function () {
+    $("aside").toggleClass("open");
+  });
+
+  $(".close-menu").click(function () {
+    $("aside").toggleClass("open");
+  });
+
   $("#edit-score").click(function () {
     $(".modal").css("display", "block");
   });
