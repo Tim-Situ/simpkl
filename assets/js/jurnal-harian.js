7bc312fe11ed881156539047a4337cfcd1214dc3
Muhammad Nur Shodiq <nursodik.2004@gmail.com>
2024-11-08 18:22:41 +0700

    Java script responsive dan dark mode

--- assets/js/jurnal-harian.js
+++ assets/js/jurnal-harian.js
@@ -54,5 +54,20 @@ $(document).ready(function () {
     $('#catatanGuru').css("display", "none"); 
     $('#catatanGuru textarea').val('');
   });
-}
-)
+
+  $(".menu-toggle").click(function () {
+    $("aside").toggleClass("open");
+  });
+  
+  $(".close-menu").click(function () {
+    $("aside").toggleClass("open");
+  });
+
+  $(".darkmode-toggle").on("click", function () {
+    $("body").toggleClass("dark-mode");
+  });
+
+  $(".close").click(function () {
+    $(".modal").css("display", "none");
+  });
+});
